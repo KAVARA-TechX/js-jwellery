0f1e5428198711a74f24eff185f3a3252c2612f4
kavaratech@gmail.com <kavaratech@gmail.com>
2021-06-12 16:55:55 +0530

    removed header card from moblie view

--- src/Components/Cards/HeaderCard.js
+++ src/Components/Cards/HeaderCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 const HeaderCard = () =>{
     return(
-        <div className="container-fluid" style={{backgroundColor : '#114063',color:'white',fontSize: "14px",fontFamily:"sans-serif"}}>
+        <div className="container-fluid mHide" style={{backgroundColor : '#114063',color:'white',fontSize: "14px",fontFamily:"sans-serif"}}>
             <div className="row">
                 <div className="tc col-4">Free shipping all over india</div>
                 <div className="tc col-5">6% off Use Code 30OFF, Ends in EXPIRED</div>
